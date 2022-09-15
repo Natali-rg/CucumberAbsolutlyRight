372f85a3db2b40becbea31dea5ce37d0c867eb22
amenson1983 <amenson1983@gmail.com>
2022-09-15 14:10:36 +0300

    Everything is fixed

--- src/main/java/cucumber/BaseCucumber.java
+++ src/main/java/cucumber/BaseCucumber.java
@@ -5,6 +5,7 @@ import driverConfig.DriverFactory;
 import org.openqa.selenium.WebDriver;
 
 public class BaseCucumber {
+
     public static WebDriver driver;
 //    public static WebDriver driver= DriverFactory.getDriver(BROWSER.CHROME);
 
